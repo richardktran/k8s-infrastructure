e90da910360af5db5915e990c82512434ec29a6b
richardktran <richardktran.dev@gmail.com>
2023-06-22 16:56:40 +0700

    add clean up containers & images

---
+++ blog-frontend/deployment/environment-dev/cleanup.groovy
@@ -0,0 +1,21 @@
+pipeline {
+  agent any
+
+  triggers {
+    cron('TZ=Asia/Saigon \n 0 * * * *') // 4AM in VietNam time
+  }
+
+  environment {
+    KUBECONFIG="$HOME/.kube/config"
+  }
+  stages {
+    stage('Clean up containers and images') {
+      steps {
+        sh '''
+        docker container prune -f | exit 0
+        docker image prune -a -f
+        '''
+      }
+    }
+  }
+}
